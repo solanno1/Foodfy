4b294bceffa8a7d33aa2ba64eb16925b62b60e7a
Solano Gordoni de Oliveira <solanno30@gmail.com>
2020-06-25 17:44:39 -0300

    Projeto finalizado. Tecnologias usadas: Javascript, NodeJS, Express, Nunjucks

--- public/script.js
+++ public/script.js
@@ -5,3 +5,28 @@ for(let card of cards){
         window.location.href = `/recipes/${recipeId}`
     })
 }
+
+// CONST DO BOTÃO "ESCONDER"
+const btnsHideShow = document.querySelectorAll(".card-hide")
+// CONST DO TEXTO 
+const txtsHideShow = document.querySelectorAll(".card-text")
+
+//RODA TODOS OS BOTÕES ESCONDER
+for (const btn of btnsHideShow) {
+    //FUNÇÃO CLICK PARA CADA BOTÃO
+    btn.addEventListener("click", function () {
+        // RECEBE EM UMA CONST O TEXTO DO BOTÃO ESCONDER
+        const btnText = btn.innerHTML;
+        // ENTRAR NO IF PROPOSITALMENTE
+        if (btnText == "ESCONDER") {
+            // ATRIBUI OUTRO TEXTO PARA O BOTÃO 
+            btn.innerHTML = "MOSTRAR";   
+            // ADICIONA A CLASSE HIDE DO CSS PARA ESCONDER TODO O ELEMENTO DO HTML DO ID EM QUAL O BOTÃO FOI ACIONADO
+            txtsHideShow[btn.id].classList.add("hide");
+        } else {
+            btn.innerHTML = "ESCONDER";  
+            txtsHideShow[btn.id].classList.remove("hide"); 
+        }
+
+    })    
+}
